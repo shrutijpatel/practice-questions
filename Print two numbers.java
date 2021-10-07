955dd58fc11511beabc103f6374cadd565c5d60b
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2021-10-07 20:48:06 +0530

    Update and rename Print two numbers to Print two numbers.java

--- Print two numbers.java
+++ Print two numbers.java
@@ -1,24 +1,24 @@
-# Print two numbers
+# Print two numbers:
 You are given two positive integers N and M. 
- You have to print the number N and M separated by a space.
+You have to print the number N and M separated by a space.
 
-# Input Format
+# Input Format:
 The first line of input contains two integer N and M.
 
-# Output Format
+# Output Format:
 Print the numbers N and M separated by a space.
 
-# Constraints
+# Constraints:
 0<=N, M<=100
 
-# Time Limit
+# Time Limit:
  0.5 second
 
-# Example
+# Example:
 Sample Input
 5 5
 
-# Sample Output
+# Sample Output:
 5 5
 ****************************************************
 
