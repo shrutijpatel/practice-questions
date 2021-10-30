76473ef69b8facdd9c0c569f7a4b042cf785b4da
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2021-10-30 21:53:47 +0530

    Create Birthday-gift.java

---
+++ Hard/Birthday-gift.java
@@ -0,0 +1,30 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-->0){
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+          if(a==b){
+            System.out.println("YES");
+          }
+          else if(a>b && c>0){
+            System.out.println("NO");
+          }
+          else if(a<b && c<0)
+            System.out.println("NO");
+          else if(c==0)
+            System.out.println("NO");
+          else if(a>b && (a-b)%c==0)
+            System.out.println("YES");
+          else if(b>a && (b-a)%c==0)
+            System.out.println("YES");
+          else
+            System.out.println("NO");
+        }
+    }
+  }
