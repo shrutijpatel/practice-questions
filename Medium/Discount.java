e954408060501e1ee9616af334d963ed05064457
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-01-16 23:45:13 +0530

    Create Discount.java

---
+++ Medium/Discount.java
@@ -0,0 +1,30 @@
+public class Discount
+{
+	public static void main(String args[])
+	{
+ 
+	double  dis,amount,markedprice,s;
+               
+	markedprice=1000;
+ 
+        dis=25;  // 25 mean 25%			
+	
+	System.out.println("markedprice= "+markedprice);
+ 
+	System.out.println("discount rate="+dis);
+         
+        s=100-dis;
+ 
+	amount= (s*markedprice)/100;
+ 
+	System.out.println("amount after discount="+amount);
+ 
+	}
+}
+
+******************************************************************
+
+Output:
+    markedprice = 1000.0
+    discount rate=25.0
+    amount after discount=750.0
