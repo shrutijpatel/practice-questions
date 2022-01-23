4711dac3d8e5aedea590b653ff0b2233ed673b7b
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-01-23 21:57:47 +0530

    Create Larrow.java

---
+++ Medium/Larrow.java
@@ -0,0 +1,47 @@
+public class  Larrow
+{
+    public static void main(String[] args)
+    {
+             
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter N : ");
+	int n=sc.nextInt();	 
+              System.out.print("Enter Symbol : ");
+              char c = sc.next().charAt(0);
+	for(int i=1;i<=n;i++) {
+	        for(int j=1;j<=n-i;j++) { 
+                  System.out.print(" ");
+          }
+		  for(int j=i;j<=n;j++)  {
+                  System.out.print(c);
+      }
+      System.out.println();
+	    }            
+      for(int i=1;i<n;i++) {
+	        for(int j=0;j<i;j++) {
+               System.out.print(" ");
+          }
+		 for(int j=0;j<=i;j++)
+     {
+        System.out.print(c);
+     } 
+     System.out.println();
+	   }    
+ }
+}
+
+***************************************************************************
+
+Output:
+Enter N :
+5
+Enter Symbol : *
+    *****
+   ****
+  ***
+ **
+*
+ **
+  ***
+   ****
+    *****
