b6c4ada575c5ba7e5183b078bc03a51030ea98e2
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-02-03 22:47:20 +0530

    Create tinaLovesA.java

---
+++ Medium/tinaLovesA.java
@@ -0,0 +1,36 @@
+import java.util.*;
+  import java.io.*;
+  
+  public class Main {
+    public static void main(String args[]) throws IOException {
+      
+      Scanner sc=new Scanner(System.in);
+      int t=sc.nextInt();
+      while(t-- >0)
+      {
+        String st=sc.next();
+        int i,count=0;
+        int len=st.length();
+        for(i=0;i<=len-1;i++)
+        {
+          char ch=st.charAt(i);
+          if(ch=='a')
+          {
+            count++;
+          }
+        }
+        int good=(len/2)+1;
+        if(count>=good)
+          System.out.println(len);
+        else
+        {
+          while(count<=good)
+          {
+            len--;
+            good=(len/2);
+          }
+          System.out.println(len);
+        }
+      }
+    }
+  }
