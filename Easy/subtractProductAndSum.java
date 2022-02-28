fa02e67f1f595039a21ec7f52b7f08882dbb35b7
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-02-28 23:34:52 +0530

    Create subtractProductAndSum.java

---
+++ Easy/subtractProductAndSum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        int product = 1;
+        int sum = 0;
+        
+        while (n > 0) {
+            int lastDigit = n % 10;
+            sum += lastDigit;
+            product *= lastDigit;
+            n /= 10;
+        }
+        
+        return (product-sum);
+    }
+}
