e4b25beb4b76820f3ba4a55a38e8a40674338376
Shruti Patel <76270372+shrutijpatel@users.noreply.github.com>
2022-06-11 22:44:23 +0530

    Create Shopping-cost.java

--- Medium/Shopping-cost.java
+++ Medium/Shopping-cost.java
@@ -1,7 +1,4 @@
-import java.util.*;
-  import java.io.*;
-  
-  public class Main {
+ public class Main {
     public static void main(String args[]) {
       Scanner sc=new Scanner(System.in);
       int n=sc.nextInt();
